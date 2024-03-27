72dd81da9655c80e079251c78cb0aa595cffca59
Enrique Diaz <enrique@nidiro.mx>
2024-03-27 01:04:33 -0600

    Generate @nidiro/ngx-map-core library

---
+++ projects/nidiro/ngx-map-core/src/lib/ngx-map-core.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nid-ngx-map-core',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ngx-map-core works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxMapCoreComponent {
+
+}
---
+++ projects/nidiro/ngx-map-core/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-map-core
+ */
+
+export * from './lib/ngx-map-core.service';
+export * from './lib/ngx-map-core.component';
