4cbd49e72bbbafb92271372c8435cbaafc2cb1e5
Valerio Galiote <val.gali@hotmail.com>
2024-04-25 02:32:54 -0600

    Integrate Angular Material - Remove unused files

--- projects/siena/src/app/map-side-panel/map-side-panel.component.ts
+++ projects/siena/src/app/map-side-panel/map-side-panel.component.ts
@@ -3,7 +3,6 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 @Component({
   selector: 'sin-map-side-panel',
   templateUrl: './map-side-panel.component.html',
-  styleUrl: './map-side-panel.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MapSidePanelComponent {
