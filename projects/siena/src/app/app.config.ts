30d5553f30754abb21564953a79a836667d05d96
Valerio Galiote <val.gali@hotmail.com>
2024-05-10 00:40:32 -0600

    Add i18n support

--- projects/siena/src/app/app.config.ts
+++ projects/siena/src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: CustomHttpLoaderFactory,
+          deps: [HttpClient],
+        },
+        useDefaultLang: true,
+      })
+    ),
+  ]
 };
+
+function CustomHttpLoaderFactory(http: HttpClient) {
+  // We use date now to prevent cache
+  return new TranslateHttpLoader(http, './assets/i18n/', `.json?v=${Date.now()}`);
+}
