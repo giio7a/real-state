c6ea121df78ee5e57325f00fa387a469ea88b5bf
Valerio Galiote <val.gali@hotmail.com>
2024-05-22 01:18:45 -0600

    Add eslint and prettier for siena project

--- projects/siena/src/app/map-side-panel/map-side-panel.component.ts
+++ projects/siena/src/app/map-side-panel/map-side-panel.component.ts
@@ -1,10 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'sin-map-side-panel',
   templateUrl: './map-side-panel.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MapSidePanelComponent {
-  isSidePanelOpen = false
+  isSidePanelOpen = false;
 }
