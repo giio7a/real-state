c04d0743c7f30df62de86f3df0f25b5828264976
Valerio Galiote <val.gali@hotmail.com>
2025-01-31 02:29:35 -0600

    27 Create UI for report - Create internal project - Add tailwind config for public project

---
+++ projects/siena-report-public/src/app/report/report.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FloatLabelModule} from 'primeng/floatlabel';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {DropdownModule} from 'primeng/dropdown';
+import {CardModule} from 'primeng/card';
+import {ButtonDirective} from 'primeng/button';
+import {Ripple} from 'primeng/ripple';
+
+@Component({
+  selector: 'srp-report',
+  standalone: true,
+  imports: [CommonModule, FloatLabelModule, InputTextModule, InputTextareaModule, DropdownModule, CardModule, ButtonDirective, Ripple],
+  templateUrl: './report.component.html',
+  styleUrl: './report.component.scss',
+})
+export class ReportComponent {
+  private localOffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
+  today = new Date(Date.now() - this.localOffset).toISOString().slice(0, 16);
+}
